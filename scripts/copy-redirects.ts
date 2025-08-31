72127953f11ed81965993ecf324c1e4a319d4c0a
Lu-Jiejie <lu-jiejie@outlook.com>
2025-08-31 23:28:08 +0800

    fix: redirects 2

--- scripts/copy-redirects.ts
+++ scripts/copy-redirects.ts
@@ -2,6 +2,7 @@ import fs from 'fs-extra'
 
 async function copyRedirects() {
   await fs.copy('_redirects', 'dist/_redirects', { overwrite: true })
+  console.log('========= Redirects copied =========')
 }
 
 copyRedirects()
